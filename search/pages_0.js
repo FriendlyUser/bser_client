329ac3fc506ccb89ef1be87d2691aa04eb1197d7
FriendlyUser <FriendlyUser@users.noreply.github.com>
2020-12-28 01:08:58 +0000

    deploy: aef41c078e7accea856f933b1f4a446211ad3c3e

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['bserclient_20documentation_19',['BserClient Documentation',['../md_ReadMe.html',1,'']]]
+  ['bserclient_20documentation_19',['BserClient Documentation',['../index.html',1,'']]]
 ];
