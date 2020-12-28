9040b5c5d71734b5118629f7810f12fcce8b4da6
David Li <davidli012345@gmail.com>
2020-12-28 04:33:11 +0000

    finishing the rank/top and rank/user endpoints

--- BserClient.Tests/BserClientTests.cs
+++ BserClient.Tests/BserClientTests.cs
@@ -3,10 +3,11 @@ using Xunit;
 using Xunit.Abstractions;
 using System.Threading.Tasks;
 using BserClient.Types;
+using System.Collections.Generic;
 namespace BserClient.Tests
 {
-  // [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-  public class BserTests : IDisposable
+    // [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class BserTests : IDisposable
     {
         private readonly ITestOutputHelper _testOutputHelper;
         // setup http client
@@ -26,17 +27,44 @@ namespace BserClient.Tests
         }
 
         [Fact]
-        public async Task Get()
+        public async Task TestGetData()
         {
-            _testOutputHelper.WriteLine("Hello");
             var bserData = await client.GetData();
+            string debugMessage = String.Format("{0} - {1}", bserData.code, bserData.message);
+            _testOutputHelper.WriteLine(debugMessage);
+            int code = bserData.code;
+            string message = bserData.message;
+            Assert.Equal(code, 200);
+            Assert.Equal(message, "Success");
+            Assert.IsType<GameDataObj>(bserData.data);
+        }
+
+        [Fact]
+        public async Task TestGetRankTop()
+        {
+            var bserData = await client.GetRankTop(1, 1);
+            // string responseBody = await response.Content.ReadAsStringAsync();
+            string debugMessage = String.Format("{0} - {1}", bserData.code, bserData.message);
+            _testOutputHelper.WriteLine(debugMessage);
+            int code = bserData.code;
+            string message = bserData.message;
+            Assert.Equal(code, 200);
+            Assert.Equal(message, "Success");
+            Assert.IsType<List<UserRankObj>>(bserData.topRanks);
+        }
+
+        [Fact]
+        public async Task TestGetRankUser()
+        {
+            // Testing first place 189543
+            var bserData = await client.GetRankUser("189543", 1, 1);
             // string responseBody = await response.Content.ReadAsStringAsync();
             _testOutputHelper.WriteLine(bserData.code.ToString());
             int code = bserData.code;
             string message = bserData.message;
             Assert.Equal(code, 200);
             Assert.Equal(message, "Success");
-            Assert.IsType<BserGameObj>(bserData.data);
+            Assert.IsType<UserRankObj>(bserData.userRank);
         }
     }
 }
