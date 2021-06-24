657a68fefe562f15d578a6c103a162b83ea48f4c
FriendlyUser <FriendlyUser@users.noreply.github.com>
2021-06-24 04:34:07 +0000

    deploy: 8bbeb27ec9a327c4614c56563eb1100058e765cb

--- search/all_0.js
+++ search/all_0.js
@@ -1,17 +1,17 @@
 var searchData=
 [
   ['bserclient_0',['BserClient',['../namespaceBserClient.html',1,'']]],
-  ['bserhttpclient_1',['BserHttpClient',['../classBserClient_1_1BserHttpClient.html',1,'BserClient.BserHttpClient'],['../classBserClient_1_1BserHttpClient.html#aa1c906f43fc394b55c6833cb140cd633',1,'BserClient.BserHttpClient.BserHttpClient()']]],
-  ['bsermetadata_2',['BserMetaData',['../classBserClient_1_1Types_1_1BserMetaData.html',1,'BserClient::Types']]],
-  ['bserranktop_3',['BserRankTop',['../classBserClient_1_1Types_1_1BserRankTop.html',1,'BserClient::Types']]],
-  ['bserrankuser_4',['BserRankUser',['../classBserClient_1_1Types_1_1BserRankUser.html',1,'BserClient::Types']]],
-  ['bserrespdata_5',['BserRespData',['../classBserClient_1_1Types_1_1BserRespData.html',1,'BserClient::Types']]],
-  ['bsertypedata_6',['BserTypeData',['../classBserClient_1_1Types_1_1BserTypeData.html',1,'BserClient::Types']]],
-  ['bserusergames_7',['BserUserGames',['../classBserClient_1_1Types_1_1BserUserGames.html',1,'BserClient::Types']]],
-  ['bserusernickname_8',['BserUserNickname',['../classBserClient_1_1Types_1_1BserUserNickname.html',1,'BserClient::Types']]],
-  ['bseruserstats_9',['BserUserStats',['../classBserClient_1_1Types_1_1BserUserStats.html',1,'BserClient::Types']]],
-  ['extensions_10',['Extensions',['../namespaceBserClient_1_1Extensions.html',1,'BserClient']]],
-  ['bserclient_20documentation_11',['BserClient Documentation',['../index.html',1,'']]],
+  ['bserclient_20documentation_1',['BserClient Documentation',['../index.html',1,'']]],
+  ['bserhttpclient_2',['BserHttpClient',['../classBserClient_1_1BserHttpClient.html',1,'BserClient.BserHttpClient'],['../classBserClient_1_1BserHttpClient.html#aa1c906f43fc394b55c6833cb140cd633',1,'BserClient.BserHttpClient.BserHttpClient()']]],
+  ['bsermetadata_3',['BserMetaData',['../classBserClient_1_1Types_1_1BserMetaData.html',1,'BserClient::Types']]],
+  ['bserranktop_4',['BserRankTop',['../classBserClient_1_1Types_1_1BserRankTop.html',1,'BserClient::Types']]],
+  ['bserrankuser_5',['BserRankUser',['../classBserClient_1_1Types_1_1BserRankUser.html',1,'BserClient::Types']]],
+  ['bserrespdata_6',['BserRespData',['../classBserClient_1_1Types_1_1BserRespData.html',1,'BserClient::Types']]],
+  ['bsertypedata_7',['BserTypeData',['../classBserClient_1_1Types_1_1BserTypeData.html',1,'BserClient::Types']]],
+  ['bserusergames_8',['BserUserGames',['../classBserClient_1_1Types_1_1BserUserGames.html',1,'BserClient::Types']]],
+  ['bserusernickname_9',['BserUserNickname',['../classBserClient_1_1Types_1_1BserUserNickname.html',1,'BserClient::Types']]],
+  ['bseruserstats_10',['BserUserStats',['../classBserClient_1_1Types_1_1BserUserStats.html',1,'BserClient::Types']]],
+  ['extensions_11',['Extensions',['../namespaceBserClient_1_1Extensions.html',1,'BserClient']]],
   ['types_12',['Types',['../namespaceBserClient_1_1Types.html',1,'BserClient']]],
   ['utils_13',['Utils',['../namespaceBserClient_1_1Utils.html',1,'BserClient']]]
 ];
